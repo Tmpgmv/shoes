bbf31ecd9081319f4a19125c9110020c9d40858a
Your Name <you@example.com>
2025-08-26 07:59:58 +0300

    tmp

--- Shoes/AppForms/OrderForm.Designer.cs
+++ Shoes/AppForms/OrderForm.Designer.cs
@@ -28,20 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 70);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.BackColor = System.Drawing.Color.Chartreuse;
+            this.splitContainer.Size = new System.Drawing.Size(800, 380);
+            this.splitContainer.SplitterDistance = 64;
+            this.splitContainer.TabIndex = 1;
             // 
             // OrderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer);
             this.Name = "OrderForm";
             this.Text = "OrderForm";
             this.Shown += new System.EventHandler(this.OrderForm_Shown);
+            this.Controls.SetChildIndex(this.splitContainer, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
     }
 }
